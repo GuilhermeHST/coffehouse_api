45ffba58d67960955d00b193d7ba205c0a10a1ae
GuilhermeHST <103433937+GuilhermeHST@users.noreply.github.com>
2022-12-19 15:13:53 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ const app = express()
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const coffeeRoutes = require("./routes/Coffee")
+const userRoutes = require("./routes/User")
 dotenv.config()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGO_URL).then(()=>{
+mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log("banco rodando");
-}).catch((erro)=>{
+}).catch((erro) => {
     console.log("erro na conexão: " + erro);
 })
 
 app.use("/coffee", coffeeRoutes)
+app.use("/user", userRoutes)
 
 app.listen(3000, () => {
     console.log("servidor rodando");
