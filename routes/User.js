f84e59dd4f39e7c954197e10269ba9d68b039a8d
GuilhermeHST <103433937+GuilhermeHST@users.noreply.github.com>
2022-12-20 14:31:47 -0300

    Add files via upload

--- routes/User.js
+++ routes/User.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express()
-const { createUser, findAllUsers, findUserById } = require("../controllers/User")
+const { createUser, findAllUsers, findUserById, updateUser, deleteUser } = require("../controllers/User")
 
 app.post("/create", createUser);
 app.get("/list", findAllUsers);
-app.get("/findById:id", findUserById);
+app.get("/findById/:id", findUserById);
+app.patch("/update/:id", updateUser);
+app.delete("delete/:id", deleteUser);
 
 module.exports = app;
